2f28fb232c71df4d2a8fcd96bf1b69a37712bd66
Raymond Johnson <raymondcmjohnson@gmail.com>
2017-06-20 17:09:17 -0500

    Basic Styling and Layout

--- js/components/holiday.js
+++ js/components/holiday.js
@@ -1,7 +1,9 @@
 angular.module('usholidays').component('holiday', {
   bindings: { holiday: '<' },
-  template: '<h3>{{$ctrl.holiday.name}}</h3>' +
+  template: '<div layout-margin>' +
+            '<h2>{{$ctrl.holiday.name}}</h2>' +
             ' <p>Date: {{$ctrl.holiday.date}}</p>' +
             ' <p>Date Observed: {{$ctrl.holiday.observed}}</p>' +
-            ' <p>Public: {{$ctrl.holiday.public}}</p>'  
+            ' <p>Public: {{$ctrl.holiday.public}}</p>' +
+            '</div>'
 });
--- js/components/holidays.js
+++ js/components/holidays.js
@@ -1,12 +1,12 @@
 angular.module('usholidays').component('holidays', {
   bindings: { holidays: '<' },
-  template: '<h3>US Holidays</h3>' +
-            '<ul>' +
-            '   <li ng-repeat="holiday in $ctrl.holidays.holidays">' +
-            '       <a ui-sref="holiday({ holidayName: holiday.name})">' +
-            '           {{holiday.name}}' +
-            '       </a>' +
-            '   </li>' +
-            '</ul>'
+  template: '<mdlist>' +
+            '   <md-list-item  ui-sref="holiday({ holidayName: holiday.name})" ng-repeat="holiday in $ctrl.holidays.holidays">' +
+            '     <span class="md-list-item-text">' +
+            '       {{holiday.name}}' +
+            '     </span>' +
+            '     <md-divider ng-if="!$last"></md-divider>' +
+            '   </md-list-item>' +
+            '</mdlist>'
 });
 
